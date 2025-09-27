69af82612c1305b17c5db98fa2d264fb49b4bc69
roshindaivik <roshin@thealteroffice.com>
2025-09-27 10:04:00 +0530

    reqline checkpoint

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,47 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net"
 
 	"github.com/roshinys/httpGo/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	buffer := make([]byte, 8)
-	ch := make(chan string, 8)
-	s := ""
-	go func() {
-		defer f.Close()
-		defer close(ch)
-		for {
-			n, err := f.Read(buffer)
-			if err != nil {
-				if s != "" {
-					ch <- s
-					// fmt.Printf("Read %s\n", s)
-				}
-				if err == io.EOF {
-					// fmt.Println("Reached end of file")
-					break
-				}
-				log.Fatal(err)
-			}
-
-			for i := 0; i < n; i++ {
-				if buffer[i] == '\n' {
-					// fmt.Printf("Read %s\n", s)
-					ch <- s
-					s = ""
-				} else {
-					s += string(buffer[i])
-				}
-			}
-		}
-	}()
-	return ch
-}
 
 func main() {
 	// Create a buffer of length 8 bytes
@@ -62,12 +26,8 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(*req)
-
-		// ch := getLinesChannel(conn)
-		// for line := range ch {
-		// 	fmt.Printf("read %s\n", line)
-		// }
+		reqLine := req.RequestLine
+		fmt.Printf("Request Line : Version : %s , Method : %s , Target : %s", reqLine.HttpVersion, reqLine.Method, reqLine.RequestTarget)
 		fmt.Println("Connection closed")
 	}
 
